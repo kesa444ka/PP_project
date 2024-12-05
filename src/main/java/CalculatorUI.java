c41b399bb36d2452e33492e72ef27c26934b0aff
Ksenia Kondrya <kesa444ka@gmail.com>
2024-12-05 21:17:24 +0300

    UI(swing) changed

--- src/main/java/CalculatorUI.java
+++ src/main/java/CalculatorUI.java
@@ -1,64 +1,26 @@
 import javax.swing.*;
-import java.awt.*;
 
-public class CalculatorUI {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Math File Processor");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
-        frame.setLayout(new GridLayout(8, 2, 10, 10));
+public class CalculatorUI extends JFrame {
+    private JLabel inputFileLabel;
+    private JLabel outputFileLabel;
+    private JTextField inputFileField;
+    private JTextField outputFileField;
+    private JLabel inputTypeLabel;
+    private JLabel outputTypeLabel;
+    private JTextField inputTypeField;
+    private JTextField outputTypeField;
+    private JLabel calcModeLabel;
+    private JLabel zipLabel;
+    private JLabel encryptLabel;
+    private JTextField calcModeField;
+    private JCheckBox zipCheckbox;
+    private JCheckBox encryptCheckbox;
+    private JButton processButton;
+    private JPanel panel;
+    private JLabel resultLabel;
 
-        JLabel inputFileLabel = new JLabel("Имя входного файла(или путь):");
-        JTextField inputFileField = new JTextField();
-
-        JLabel outputFileLabel = new JLabel("Имя выходного файла(или путь):");
-        JTextField outputFileField = new JTextField();
-
-        JLabel inputTypeLabel = new JLabel("Тип входного файла (txt/json/xml/yaml):");
-        JTextField inputTypeField = new JTextField();
-
-        JLabel outputTypeLabel = new JLabel("Тип выходного файла (txt/json/xml/yaml):");
-        JTextField outputTypeField = new JTextField();
-
-        JLabel calcModeLabel = new JLabel("Способ расчёта(1/2/3)");
-        JTextField calcModeField = new JTextField();
-
-        JLabel zipLabel = new JLabel("Архивировать ли выходной файл? (true/false):");
-        JCheckBox zipCheckbox = new JCheckBox();
-
-        JLabel encryptLabel = new JLabel("Шифровать ли выходной файл? (true/false):");
-        JCheckBox encryptCheckbox = new JCheckBox();
-
-        JButton processButton = new JButton("Начать работу!");
-        JTextArea resultArea = new JTextArea();
-        resultArea.setEditable(false);
-        resultArea.setLineWrap(true);
-
-        frame.add(inputFileLabel);
-        frame.add(inputFileField);
-
-        frame.add(outputFileLabel);
-        frame.add(outputFileField);
-
-        frame.add(inputTypeLabel);
-        frame.add(inputTypeField);
-
-        frame.add(outputTypeLabel);
-        frame.add(outputTypeField);
-
-        frame.add(calcModeLabel);
-        frame.add(calcModeField);
-
-        frame.add(zipLabel);
-        frame.add(zipCheckbox);
-
-        frame.add(encryptLabel);
-        frame.add(encryptCheckbox);
-
-        frame.add(processButton);
-        frame.add(new JScrollPane(resultArea));
-
-        processButton.addActionListener(e -> {
+    public CalculatorUI() {
+        processButton.addActionListener(actionEvent -> {
             try {
                 String inputFile = inputFileField.getText();
                 String outputFile = outputFileField.getText();
@@ -86,13 +48,19 @@ public class CalculatorUI {
                         String.valueOf(b.getShouldZip()),
                         String.valueOf(b.getShouldEncrypt())
                 });
-
-                resultArea.setText("Работа завершена успешно!");
+                resultLabel.setText("Работа завершена успешно!");
             } catch (Exception ex) {
-                resultArea.setText("Error: " + ex.getMessage());
+                resultLabel.setText("Error: " + ex.getMessage());
             }
         });
+    }
 
+    public static void main(String[] args) {
+        CalculatorUI frame = new CalculatorUI();
+        frame.setContentPane(frame.panel);
+        frame.setTitle("ArraySort");
+        frame.setSize(700, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 }
